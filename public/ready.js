ebcb320790e840b6d4cac1bb32ab3c0afdf98cc1
KathiaNeira <kathianeira@outlook.com>
2016-06-15 16:33:30 -0500

    Update ready.js

--- public/ready.js
+++ public/ready.js
@@ -1,48 +1,49 @@
 $(document).ready(function() {
-    var ordenarNumeros = function(){
-		var dom = {};
-		var st = {
-    	array : [9,3,1,4,5,2,7,8,6],
-      borderAnimate : ".numeros"
+  var ordenarNumeros = function() {
+    var dom = {};
+    var st = {
+      arreglo: [9, 3, 1, 4, 5, 2, 7, 8, 6],
+      borderAnimate: ".numeros"
     };
-		var catchDom = function(){
-      dom.array = $(st.array);
+    var catchDom = function() {
       dom.borderAnimate = $(st.borderAnimate);
     };
-		var afterCatchDom = function(){
+    var afterCatchDom = function() {
       functions.mostrarNumeros();
-      functions.ordenarNumeros();
-      functions.animateOrden();
+      functions.ordenarNumeros(st.arreglo);
     };
     window.setInterval(function() {
       $(".numeros").toggleClass('active');
     }, 1000);
     var functions = {
-        mostrarNumeros:function(){
-        	array = [9,3,1,4,5,2,7,8,6];
-           array.forEach(function (item, index, array) {
-            $(".numeros").append('<div class="numerosOrder">'+item+'</div>');
-          });
-        },
-        ordenarNumeros:function(){
-        	console.log("ordenar los numeros");
-          dom.array.sort(function(a, b) {
-            return a - b;
-          });
-          console.log(dom.array);
-        },
-        animateOrden:function(){
-          console.log("animacion de orden");
+      mostrarNumeros: function() {
+        array = [9, 3, 1, 4, 5, 2, 7, 8, 6];
+        array.forEach(function(item, index, array) {
+          $(".numeros").append('<div>' + item + '</div>');
+        });
+      },
+      ordenarNumeros: function(arreglo) {
+        console.log("animacion de orden");
+        for (var i = 0; i < arreglo.length ; i++) {
+          for (var j = 0; j < arreglo.length - i ; j++) {
+            if (arreglo[j] > arreglo[j + 1]) {
+              var aux = arreglo[j];
+              arreglo[j] = arreglo[j + 1];
+              arreglo[j + 1] = aux;
+            }
+          }
         }
+        console.log(arreglo);
+      }
     };
 
-		var initialize = function(){
-			catchDom();
-			afterCatchDom();
-		};
-		return{
-			init: initialize
-		};
-	};
-	ordenarNumeros().init();
+    var initialize = function() {
+      catchDom();
+      afterCatchDom();
+    };
+    return {
+      init: initialize
+    };
+  };
+  ordenarNumeros().init();
 });
