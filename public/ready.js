7a18e1eeb75af6568af8ebe969ab405e5d1c06db
kathia <kathia.neira@orbis.com.pe>
2016-09-29 18:23:57 -0500

    feat(ordenar):corrigiendo js

--- public/ready.js
+++ public/ready.js
@@ -2,7 +2,10 @@
   var dom = {};
   var st = {
     arreglo: [3, 1, 9, 4, 5, 2, 7, 8, 6],
-    borderAnimate: ".numeros"
+    borderAnimate: ".numeros",
+    i : 0,
+    j : 0,
+    time : ''
   };
 
   var catchDom = function() {
@@ -11,11 +14,11 @@
 
   var afterCatchDom = function() {
     functions.mostrarNumeros();
-    functions.ordenarNumeros(st.arreglo);
+    st.time = setInterval(functions.ordenarNumeros, 2000);
   };
-
   var functions={};
 
+  //Mostrando el array inicial
   functions.mostrarNumeros=()=>{
     array = [3, 1, 9, 4, 5, 2, 7, 8, 6];
     array.forEach(function(item, index, array) {
@@ -23,82 +26,70 @@
     });
   };
 
-  functions.ordenarNumeros=(j)=>{
-    for (var i = 0; i < st.arreglo.length ; i++) {
-      console.log('st.arreglo desordenado1',st.arreglo);
-      for (var j = 0; j < st.arreglo.length - i ; j++) {
-        //alert(st.arreglo[j]+'es mayor a'+st.arreglo[j+1]);
-        functions.animateNumber(j);
-        if (st.arreglo[j] > st.arreglo[j + 1]) {
-          var aux = st.arreglo[j];
-          st.arreglo[j] = st.arreglo[j + 1];
-          st.arreglo[j + 1] = aux;
-          continue;
-          //console.log("#animando - cambiando posiciones", st.arreglo);
-          //return;
-          //return false;
-        }
-        console.log('este es el nuevo arreglo', st.arreglo)
-        break;
+  //Método burbuja para el recorrido del array
+  functions.ordenarNumeros=()=>{
+    if (st.i < st.arreglo.length) {
+      if (st.j < st.arreglo.length - st.i) {
+        functions.compareNumPrevToNext()
+        st.j++;
+      } else{
+        st.j = 0;
+        st.i++;
       }
-
-      //break;
-      console.log('aquí se hace la animación', st.arreglo);
-    }
-    console.log('st.arreglo ordenado',st.arreglo);
-  };
-
-  functions.animateNumber=(j)=>{
-    if (st.arreglo[j] > st.arreglo[j + 1]) {
-      //console.log('st.arreglo[j]', st.arreglo[j], '> st.arreglo[j + 1]', st.arreglo[j + 1]);
-      //console.log('es verdad');
-
-      var indexofMenorNumber =  st.arreglo.indexOf(st.arreglo[j]);
-      var indexofMayorNumber =  st.arreglo.indexOf(st.arreglo[j+1]);
-      var menorNumber = $('.numberItem').eq(indexofMayorNumber);
-      var mayorNumber =$('.numberItem').eq(indexofMenorNumber);
-      console.log("->>", menorNumber, 'mayorNumber', mayorNumber);
-
-      // var positionMayorNumber = menorNumber.position();;
-      var positionMenorNumber = menorNumber.position();
-      var positionMayorNumber = mayorNumber.position();
-      var left = positionMenorNumber.left;
-
-      var distance = positionMenorNumber.left-positionMayorNumber.left +13;
-      //console.log('distance', distance)
-      menorNumber.animate({
-        "top" : "-80"
-      }, 2000, function(){
-        mayorNumber.animate({
-          "left" : distance
-        }, 2000, function(){
-          menorNumber.animate({
-            "right": distance,
-            "top": '+=80'
-          }, 2000);
-        });
-      });
-      //
-      //console.log('positionMenorNumber', positionMenorNumber.left, 'positionMayorNumber', positionMayorNumber.left)
-
-
-      menorNumber.css('border', '5px solid red');
-      mayorNumber.css('border', '5px solid black');
-
-
     }else{
-      console.log('es falso');
+      clearInterval(st.time);
+    }
+    functions.accommodateNumber();
+  };
+
+  //Comparando número con número para utilizarlo en el método burbuja
+  functions.compareNumPrevToNext=()=>{
+    if (st.arreglo[st.j] > st.arreglo[st.j + 1]) {
+      var aux = st.arreglo[st.j];
+      st.arreglo[st.j] = st.arreglo[st.j + 1];
+      st.arreglo[st.j + 1] = aux;
     }
   };
 
-  functions.accommodateNumber=()=>{
-    console.log("#animando - cambiando posiciones===>", st.arreglo);
-    $(".numeros").empty()
-    st.arreglo.forEach(function(item, index) {
-      $(".numeros").append('<div class="numberItem">' + item + '</div>');
-    });
+  //Animación para pintar el borde de rojo o de negro
+  functions.animateNumberColor=()=>{
+    var indexofMenorNumber =  st.arreglo.indexOf(st.arreglo[st.j]);
+    var indexofMayorNumber =  st.arreglo.indexOf(st.arreglo[st.j+1]);
+    var menorNumber = $('.numberItem').eq(indexofMayorNumber);
+    var mayorNumber =$('.numberItem').eq(indexofMenorNumber);
 
-  }
+    // var positionMayorNumber = menorNumber.position();;
+    var positionMenorNumber = menorNumber.position();
+    var positionMayorNumber = mayorNumber.position();
+    var left = positionMenorNumber.left;
+
+    var distance = positionMenorNumber.left-positionMayorNumber.left +13;
+    menorNumber.animate({
+      "top" : "-80"
+    }, 2000, function(){
+      mayorNumber.animate({
+        "left" : distance
+      }, 2000, function(){
+        menorNumber.animate({
+          "right": distance,
+          "top": '+=80'
+        }, 2000);
+      });
+    });
+    menorNumber.css('border', '5px solid red');
+    mayorNumber.css('border', '5px solid black');
+  };
+
+
+  //Reutilizando la función para hacerla una y otra vez
+  functions.accommodateNumber=()=>{
+    $(".numeros").empty();
+    for (var x = 0; x < st.arreglo.length; x++) {
+        functions.animateNumberColor();
+        //$(".numeros").append('<div class="numberItem">' + st.arreglo[x] + '</div>');
+    }
+
+  };
 
   var initialize = function() {
     catchDom();
