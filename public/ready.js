2b01d16e1cf10d09fb60718ef2bd65e6e58f570e
kathia <kathia.neira@orbis.com.pe>
2016-10-03 18:47:00 -0500

    feat(animaciones):corriegiendo animaciones

--- public/ready.js
+++ public/ready.js
@@ -1,11 +1,12 @@
 (function(){
   var dom = {};
   var st = {
-    arreglo: [3, 1, 9, 4, 5, 2, 7, 8, 6],
+    arreglo: [6, 1, 9, 4,8],
     borderAnimate: ".numeros",
     i : 0,
     j : 0,
-    time : ''
+    time : '',
+    positions:[]
   };
 
   var catchDom = function() {
@@ -13,15 +14,21 @@
   };
 
   var afterCatchDom = function() {
+    console.log("afterCatchDom");
     functions.mostrarNumeros();
-    st.time = setInterval(functions.ordenarNumeros, 500);
+    $(".numberItem").each(function(e,b){
+        //console.log(e, $(b).position());
+        st.positions.push($(b).position());
+      }
+    );
+    console.log("array de posiciones", st.positions);
+    st.time = setInterval(functions.ordenarNumeros, 1000);
   };
   var functions={};
 
   //Mostrando el array inicial
   functions.mostrarNumeros=()=>{
-    array = [3, 1, 9, 4, 5, 2, 7, 8, 6];
-    array.forEach(function(item, index, array) {
+    st.arreglo.forEach(function(item, index, array) {
       $(".numeros").append('<div class="numberItem">' + item + '</div>');
     });
   };
@@ -29,10 +36,8 @@
   //Método burbuja para el recorrido del array
   functions.ordenarNumeros=()=>{
     if (st.i < st.arreglo.length) {
-      //functions.accommodateNumber();
       if (st.j < st.arreglo.length - st.i) {
         functions.compareNumPrevToNext()
-        functions.animateNumberColor();
         st.j++;
       } else{
         st.j = 0;
@@ -45,64 +50,86 @@
 
   //Comparando número con número para utilizarlo en el método burbuja
   functions.compareNumPrevToNext=()=>{
+    // actualizar nuevas posiciones
     if (st.arreglo[st.j] > st.arreglo[st.j + 1]) {
+      //
+      console.log('se hace la animación antinguo array', st.arreglo);
+      var prevMayor = st.arreglo[st.j];
+      var prevMenor = st.arreglo[st.j + 1];
+      console.log("numeros", prevMayor, prevMenor)
+      var indices = functions.getIndices(prevMayor, prevMenor);
+      console.log("indices", indices);
+      // animacion
+      functions.animation(indices);
+      //
       var aux = st.arreglo[st.j];
       st.arreglo[st.j] = st.arreglo[st.j + 1];
       st.arreglo[st.j + 1] = aux;
+      console.log('se hace la animación actualizando array', st.arreglo);
+    }else{
+      console.log('no se hace la animacion');
     }
   };
-
-  //Animación para pintar el borde de rojo o de negro
-  functions.animateNumberColor=()=>{
-    var indexofMenorNumber =  st.arreglo.indexOf(st.arreglo[st.j]);
-    var indexofMayorNumber =  st.arreglo.indexOf(st.arreglo[st.j+1]);
-    var menorNumber = $('.numberItem').eq(indexofMayorNumber);
-    var mayorNumber =$('.numberItem').eq(indexofMenorNumber);
-
-    console.log('medida menorNumber', menorNumber.width())
-    console.log("menorNumber", menorNumber);
-    console.log("mayorNumber", mayorNumber);
-    //var positionMayorNumber = menorNumber.position();;
-    var positionMenorNumber = menorNumber.position();
-    var positionMayorNumber = mayorNumber.position();
-    var left = positionMenorNumber.left;
-
-  //  console.log('letfMenorNum', letfMenorNum)
-    //console.log('positionMayorNumber', positionMayorNumber, 'positionMenorNumber', positionMenorNumber)
-
-    var ditanceRight = positionMenorNumber+positionMenorNumber
-
-    var distance = positionMenorNumber.left-positionMayorNumber.left;
-    var total=0;
-    total+=distance;
-    console.log('total', total);
-
-    console.log('distance', distance);
-    // menorNumber.animate({
-    //   "top" : "-80"
-    // }, 200, function(){
-    //   mayorNumber.animate({
-    //     "left" : distance
-    //   }, 200, function(){
-    //     menorNumber.animate({
-    //       "right": letfMenorNum,
-    //       "top": '+=80'
-    //     }, 200);
-    //   });
-    // });
-    menorNumber.css('border', '5px solid red');
-    mayorNumber.css('border', '5px solid black');
+  functions.getIndices = (mayor, menor)=>{
+      //console.log("getIndices", mayor, menor)
+      var IndiceMayor =  st.arreglo.indexOf(mayor);
+      var IndiceMenor =  st.arreglo.indexOf(menor);
+      //console.log("getIndices==>", mayor, menor)
+      return {
+          mayor:IndiceMayor,
+          menor:IndiceMenor
+      };
   };
+  functions.animation = (indices) => {
+    var mayor = $('.numberItem').eq(indices.mayor);
+    var menor = $('.numberItem').eq(indices.menor);
+    console.log('elementos que se van a animar', mayor, menor);
 
-
-  //Reutilizando la función para hacerla una y otra vez
-  functions.accommodateNumber=()=>{
-    //$(".numeros").empty();
-    console.log('st.arreglo[st.j]', st.arreglo[st.j], 'st.arreglo[st.j+1]', st.arreglo[st.j+1]);
-    console.log('hacemos la animacion');
-    functions.animateNumberColor();
+    mayor.css({
+      right: -60
+    });
+    menor.css({
+      left: -60
+    })
     return false;
+
+
   };
+  //Animación para acomodar el nuevo array
+  // functions.animateNumberColor=()=>{
+  //   console.log('comparamos los números', st.arreglo[st.j], 'y', st.arreglo[st.j+1]);
+  //
+  //   var indexofPrevNumber =  st.arreglo.indexOf(st.arreglo[st.j]);
+  //   var indexofNextNumber =  st.arreglo.indexOf(st.arreglo[st.j+1]);
+  //   var nextNumber = $('.numberItem').eq(indexofNextNumber);
+  //   var prevNumber = $('.numberItem').eq(indexofPrevNumber);
+  //   console.log('indexofPrevNumber', indexofPrevNumber, 'indexofNextNumber', indexofNextNumber)
+  //   //console.log('hacemos la animacion');
+  //
+  //   console.log('nextNumber', nextNumber.position().left, 'prevNumber', prevNumber.position().left, 'resultado', nextNumber.position().left-prevNumber.position().left);
+  //
+  //
+  //   nextNumber.animate({
+  //     'top': '-80px'
+  //   }, 1000, function(){
+  //     prevNumber.animate({
+  //       "left" : '60px'
+  //     }, 1000, function(){
+  //       nextNumber.animate({
+  //          "right": '60px',
+  //          "top": '+=80'
+  //        }, 1000);
+  //     });
+  //   });
+  //
+  //   // nextNumber.css('background', 'yellow');
+  //   // prevNumber.css('background', 'pink');
+  //
+  //   nextNumber.css('border', '5px solid red');
+  //   //prevNumber.css('border', '5px solid black');
+  // //   nextNumber.removeAttr('style');
+  // //   prevNumber.removeAttr('style');
+  //  };
 
   var initialize = function() {
     catchDom();
