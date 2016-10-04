fb350c95db9ebd1027cdcc54660b18767a5a9814
kathia <kathia.neira@orbis.com.pe>
2016-10-04 18:37:12 -0500

    feat(animacion):corrigiendo animacion

--- public/ready.js
+++ public/ready.js
@@ -1,7 +1,7 @@
 (function(){
   var dom = {};
   var st = {
-    arreglo: [6, 1, 9, 4,8],
+    arreglo: [6, 5, 3, 1, 8, 7, 2, 4],
     borderAnimate: ".numeros",
     i : 0,
     j : 0,
@@ -14,15 +14,8 @@
   };
 
   var afterCatchDom = function() {
-    console.log("afterCatchDom");
     functions.mostrarNumeros();
-    $(".numberItem").each(function(e,b){
-        //console.log(e, $(b).position());
-        st.positions.push($(b).position());
-      }
-    );
-    console.log("array de posiciones", st.positions);
-    st.time = setInterval(functions.ordenarNumeros, 1000);
+    st.time = setInterval(functions.ordenarNumeros, 3000);
   };
   var functions={};
 
@@ -35,6 +28,7 @@
 
   //Método burbuja para el recorrido del array
   functions.ordenarNumeros=()=>{
+    functions.emptyNum();
     if (st.i < st.arreglo.length) {
       if (st.j < st.arreglo.length - st.i) {
         functions.compareNumPrevToNext()
@@ -48,96 +42,57 @@
     }
   };
 
+  //Limpiando los números para sacar las nuevas posiciones
+  functions.emptyNum=()=>{
+    $('.numeros').empty();
+    functions.mostrarNumeros();
+  };
+
   //Comparando número con número para utilizarlo en el método burbuja
   functions.compareNumPrevToNext=()=>{
-    // actualizar nuevas posiciones
     if (st.arreglo[st.j] > st.arreglo[st.j + 1]) {
-      //
-      console.log('se hace la animación antinguo array', st.arreglo);
-      var prevMayor = st.arreglo[st.j];
-      var prevMenor = st.arreglo[st.j + 1];
-      console.log("numeros", prevMayor, prevMenor)
-      var indices = functions.getIndices(prevMayor, prevMenor);
-      console.log("indices", indices);
-      // animacion
-      functions.animation(indices);
-      //
+      functions.animation();
       var aux = st.arreglo[st.j];
       st.arreglo[st.j] = st.arreglo[st.j + 1];
       st.arreglo[st.j + 1] = aux;
-      console.log('se hace la animación actualizando array', st.arreglo);
     }else{
-      console.log('no se hace la animacion');
+      console.log('no se hace la animacion pero se tiene que eliminar el tiempo');
     }
   };
-  functions.getIndices = (mayor, menor)=>{
-      //console.log("getIndices", mayor, menor)
-      var IndiceMayor =  st.arreglo.indexOf(mayor);
-      var IndiceMenor =  st.arreglo.indexOf(menor);
-      //console.log("getIndices==>", mayor, menor)
-      return {
-          mayor:IndiceMayor,
-          menor:IndiceMenor
-      };
+  // Animando los dos números que se están comparando
+  functions.animation = () => {
+    var indexofPrevNumber =  st.arreglo.indexOf(st.arreglo[st.j]);
+    var indexofNextNumber =  st.arreglo.indexOf(st.arreglo[st.j+1]);
+    var nextNumber = $('.numberItem').eq(indexofNextNumber);
+    var prevNumber = $('.numberItem').eq(indexofPrevNumber);
+
+    var positionNext = nextNumber.position().left;
+    var positionPrev = prevNumber.position().left;
+    var resta = positionNext-positionPrev;
+
+    nextNumber.animate({
+     'top': '-80px'
+   }, 1000, function(){
+     prevNumber.animate({
+       "left" : resta
+     }, 1000, function(){
+       nextNumber.animate({
+          "right": resta,
+          "top": '+=80'
+        }, 1000);
+     });
+   });
+
+   nextNumber.css('border', '5px solid red');
+   prevNumber.css('border', '5px solid black');
+
   };
-  functions.animation = (indices) => {
-    var mayor = $('.numberItem').eq(indices.mayor);
-    var menor = $('.numberItem').eq(indices.menor);
-    console.log('elementos que se van a animar', mayor, menor);
-
-    mayor.css({
-      right: -60
-    });
-    menor.css({
-      left: -60
-    })
-    return false;
-
-
-  };
-  //Animación para acomodar el nuevo array
-  // functions.animateNumberColor=()=>{
-  //   console.log('comparamos los números', st.arreglo[st.j], 'y', st.arreglo[st.j+1]);
-  //
-  //   var indexofPrevNumber =  st.arreglo.indexOf(st.arreglo[st.j]);
-  //   var indexofNextNumber =  st.arreglo.indexOf(st.arreglo[st.j+1]);
-  //   var nextNumber = $('.numberItem').eq(indexofNextNumber);
-  //   var prevNumber = $('.numberItem').eq(indexofPrevNumber);
-  //   console.log('indexofPrevNumber', indexofPrevNumber, 'indexofNextNumber', indexofNextNumber)
-  //   //console.log('hacemos la animacion');
-  //
-  //   console.log('nextNumber', nextNumber.position().left, 'prevNumber', prevNumber.position().left, 'resultado', nextNumber.position().left-prevNumber.position().left);
-  //
-  //
-  //   nextNumber.animate({
-  //     'top': '-80px'
-  //   }, 1000, function(){
-  //     prevNumber.animate({
-  //       "left" : '60px'
-  //     }, 1000, function(){
-  //       nextNumber.animate({
-  //          "right": '60px',
-  //          "top": '+=80'
-  //        }, 1000);
-  //     });
-  //   });
-  //
-  //   // nextNumber.css('background', 'yellow');
-  //   // prevNumber.css('background', 'pink');
-  //
-  //   nextNumber.css('border', '5px solid red');
-  //   //prevNumber.css('border', '5px solid black');
-  // //   nextNumber.removeAttr('style');
-  // //   prevNumber.removeAttr('style');
-  //  };
 
   var initialize = function() {
     catchDom();
     afterCatchDom();
   };
-
   return {
     init: initialize
   };
-
 })().init();
