b7f012f540f17dbb12a70c50a2120efd5866194f
kathia <kathia.neira@orbis.com.pe>
2016-10-05 10:17:44 -0500

    feat(gitignore):agregando gitignore

--- serverExpress.js
+++ serverExpress.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 app.get('/',function(request, response){
   response.sendFile(__dirname + '/index.html');
 });
